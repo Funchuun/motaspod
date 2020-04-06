151415ece7e604ef66a22b5667434ed97a6fd986
Funchuun <kobf1w07@gmail.com>
2020-04-06 11:27:35 +0900

    tweet function phase1

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,10 +9,19 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
   end
 
   private
   def tweet_params
-    params.require(:tweet).permit(:image, :carname, :year, :description)
+    params.require(:tweet).permit(:image, :carname, :year, :description).merge(user_id: current_user.id)
   end
 end
