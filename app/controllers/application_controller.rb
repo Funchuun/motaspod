22c9130da2cf469e5f26029ef6e62d8ffe4f4ac4
Funchuun <kobf1w07@gmail.com>
2020-05-16 22:43:28 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
+
   protected
 
   def configure_permitted_parameters
